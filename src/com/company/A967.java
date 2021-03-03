fc9a093cf49051ee8e62768f2daaeab407ba4338
M1rirx <mir.allaberganov@gmail.com>
2021-03-03 16:16:19 +0500

    commit 3

--- src/com/company/A967.java
+++ src/com/company/A967.java
@@ -17,28 +17,19 @@ public class A967 {
         for (int i = 0; i < n; i++) {
             long a = sc.nextLong();
             b[i] = s(a, k1) * s(a, k2);
-            pw.print(b[i] + " ");
         }
-
-        for (long item : b) pw.print(item + " ");
-        pw.println("");
-
-        Arrays.sort(b);
-
-        for (long item : b) pw.print(item + " ");
-        pw.println("");
-
-        for (long item : b) pw.print(item + " ");
-
+        Arrays.sort(b);                           //sort
+        for (int i = b.length - 1; i >= 0; i--)  // reverse print of sorted array
+            pw.print(b[i] + " ");
         pw.flush();
     }
 
     public static long s(long x, int k) {
-        long summ = 0;
+        long sum = 0;
         while (x != 1) {
-            summ += x % k;
+            sum += x % k;
             x /= k;
         }
-        return summ;
+        return sum;
     }
 }
